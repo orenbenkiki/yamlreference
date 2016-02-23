531843c19264568bbae9e73546811464905e8554
Oren Ben-Kiki <oren@ben-kiki.org>
2016-02-23 09:57:44 +0159

    Do not combine surrogate pairs in UTF32.

--- Text/Yaml/Reference.hs
+++ Text/Yaml/Reference.hs
@@ -139,8 +139,8 @@ undoEncoding encoding bytes =
     UTF8    -> undoUTF8 bytes 0
     UTF16LE -> combinePairs $ undoUTF16LE bytes 0
     UTF16BE -> combinePairs $ undoUTF16BE bytes 0
-    UTF32LE -> combinePairs $ undoUTF32LE bytes 0
-    UTF32BE -> combinePairs $ undoUTF32BE bytes 0
+    UTF32LE -> undoUTF32LE bytes 0
+    UTF32BE -> undoUTF32BE bytes 0
 
 -- ** UTF-32 decoding
 
